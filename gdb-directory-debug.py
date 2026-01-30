70cd6ee818b5dc84eac9c75d1888e11469a965fc
Ryan Govostes <rgovostes+git@gmail.com>
2026-01-30 10:50:52 +0900

    asdfasdfag

--- gdb-directory-debug.py
+++ gdb-directory-debug.py
@@ -4,19 +4,19 @@ def get_errno():
     # Casting to function pointer ensures we call the thread-local errno location correctly
     return int(gdb.parse_and_eval("*((int * (*) (void)) __errno_location) ()"))
 
-def get_ret_reg():
-    arch = gdb.selected_frame().architecture().name()
-    return "$x0" if "aarch64" in arch else "$rax"
-
 class ReaddirReturnBreakpoint(gdb.FinishBreakpoint):
     """Inner-most hook: Captures the result of readdir."""
     def __init__(self):
         super().__init__(internal=True)
 
     def stop(self):
-        ret = int(gdb.parse_and_eval(get_ret_reg()))
+        try:
+            response = gdb.parse_and_eval("((struct dirent *)$x0).d_name").string()
+            response = f"\"{response}\""
+        except gdb.error:
+            response = "(null)"
         errno = get_errno()
-        print(f"    [+] readdir ret={hex(ret)}, errno={errno}")
+        print(f"    [+] readdir {response}, errno={errno}")
         return False
 
 class ReaddirBreakpoint(gdb.Breakpoint):
@@ -69,7 +69,7 @@ class FindLibraryNameBreakpoint(gdb.Breakpoint):
 
     def stop(self):
         # Let this throw if 'name' or 'Raw' isn't in scope/valid
-        name_str = gdb.parse_and_eval("name.Raw").string()
+        name_str = gdb.parse_and_eval("name.Raw.c_str()").string()
         
         if "rcutils" in name_str:
             print(f"\n[TARGET DETECTED] Processing: {name_str}")
@@ -80,3 +80,9 @@ class FindLibraryNameBreakpoint(gdb.Breakpoint):
 
 
 FindLibraryNameBreakpoint()
+
+
+try:
+    gdb.execute("run")
+except gdb.error:
+    gdb.execute("continue")
