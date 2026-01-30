cedf29cf1a22bad6ff12e4f11b0f0677613246c9
Ryan Govostes <rgovostes+git@gmail.com>
2026-01-30 09:48:07 +0900

    loasdf

--- gdb-directory-debug.py
+++ gdb-directory-debug.py
@@ -1,73 +1,82 @@
 import gdb
 
 def get_errno():
-    try:
-        return int(gdb.parse_and_eval("*(int*)__errno_location()"))
-    except:
-        return None
+    # Casting to function pointer ensures we call the thread-local errno location correctly
+    return int(gdb.parse_and_eval("*((int * (*) (void)) __errno_location) ()"))
 
+def get_ret_reg():
+    arch = gdb.selected_frame().architecture().name()
+    return "$x0" if "aarch64" in arch else "$rax"
 
 class ReaddirReturnBreakpoint(gdb.FinishBreakpoint):
-    def __init__(self,):
+    """Inner-most hook: Captures the result of readdir."""
+    def __init__(self):
         super().__init__(internal=True)
 
     def stop(self):
+        ret = int(gdb.parse_and_eval(get_ret_reg()))
         errno = get_errno()
-        ret = int(gdb.parse_and_eval("$x0"))
-        print(f"readdir ret={hex(ret)}, errno={errno}")
+        print(f"    [+] readdir ret={hex(ret)}, errno={errno}")
         return False
 
-    def out_of_scope(self):
-        pass
-
-
 class ReaddirBreakpoint(gdb.Breakpoint):
+    """Middle-hook: Watches calls to readdir within Directory::Load."""
     def __init__(self):
-        super().__init__("readdir", gdb.BP_BREAKPOINT)
+        super().__init__("readdir", gdb.BP_BREAKPOINT, internal=True)
 
     def stop(self):
         ReaddirReturnBreakpoint()
         return False
 
-
 class DirectoryLoadExitBreakpoint(gdb.FinishBreakpoint):
-    def __init__(self):
+    """Cleans up the readdir hook when Directory::Load returns."""
+    def __init__(self, readdir_bp):
         super().__init__(internal=True)
+        self.readdir_bp = readdir_bp
 
     def stop(self):
-        print(f"<<< Directory::Load returned")
-
-        global readdir_bp
-        readdir_bp.delete()
-        readdir_bp = None
-
+        if self.readdir_bp.is_valid():
+            self.readdir_bp.delete()
         return False
-
 
 class DirectoryLoadBreakpoint(gdb.Breakpoint):
+    """Triggered inside the target library search."""
     def __init__(self):
-        super().__init__(
-            "cmsys::Directory::Load",
-            gdb.BP_BREAKPOINT,
-            internal=True
-        )
+        super().__init__("cmsys::Directory::Load", gdb.BP_BREAKPOINT, internal=True)
 
     def stop(self):
-        print(f">>> Entering Directory::Load")
-        DirectoryLoadExitBreakpoint()
+        print(f"  >>> Entering Directory::Load")
+        rbp = ReaddirBreakpoint()
+        DirectoryLoadExitBreakpoint(rbp)
+        return False
 
-        global readdir_bp
-        readdir_bp = ReaddirBreakpoint()
+class CheckDirectoryExitBreakpoint(gdb.FinishBreakpoint):
+    """Outer-most cleanup: Removes the Load hook when the library check finishes."""
+    def __init__(self, load_bp):
+        super().__init__(internal=True)
+        self.load_bp = load_bp
 
+    def stop(self):
+        print(f"<<< Leaving CheckDirectoryForName scope.")
+        if self.load_bp.is_valid():
+            self.load_bp.delete()
+        return False
+
+class FindLibraryNameBreakpoint(gdb.Breakpoint):
+    """The Master Filter."""
+    def __init__(self):
+        super().__init__("cmFindLibraryHelper::CheckDirectoryForName", gdb.BP_BREAKPOINT)
+
+    def stop(self):
+        # Let this throw if 'name' or 'Raw' isn't in scope/valid
+        name_str = gdb.parse_and_eval("name.Raw").string()
+        
+        if "rcutils" in name_str:
+            print(f"\n[TARGET DETECTED] Processing: {name_str}")
+            lbp = DirectoryLoadBreakpoint()
+            CheckDirectoryExitBreakpoint(lbp)
+            
         return False
 
 
-readdir_bp = None
-
-DirectoryLoadBreakpoint()
-
-# Try run (local), fall back to continue (remote)
-try:
-    gdb.execute("run")
-except gdb.error:
-    gdb.execute("continue")
+FindLibraryNameBreakpoint()
